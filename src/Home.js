e9be010317916feccad57932bc901fbd8ad60840
Tuti Wulandari <tutiwulandari150@gmail.com>
2021-06-16 16:29:43 +0700

    add simple navigation

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+
+export class Home extends Component {
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <View style={{marginTop:20, justifyContent:'center'}}>
+          <Text style={{fontSize:28,textAlign:'center'}}> This is My Home</Text>
+        </View>
+        <TouchableOpacity style={{marginTop:20,borderRadius:10, elevation:10, paddingHorizontal:10, paddingVertical:10}}>
+          <Text style={{fontSize:24}}> PINDAH</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+});
